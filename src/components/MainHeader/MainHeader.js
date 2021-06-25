6b943275f90bf63cfd253b2d4b8342179cc8427e
Srijana Thulung <srijana.thulung123@gmail.com>
2021-06-25 10:26:36 +0545

    MainHeader page is added

---
+++ src/components/MainHeader/MainHeader.js
@@ -0,0 +1,11 @@
+import classes from './MainHeader.module.css'
+import React from 'react';
+
+const MainHeader = () => {
+    return (
+         <header className={classes['main-header']}>
+            <h1>This is a typical page</h1>
+        </header>
+    )
+}
+export default MainHeader;
