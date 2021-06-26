cf9ce309eadfc12db85b83ea3cab3032d44447b8
Srijana Thulung <srijana.thulung123@gmail.com>
2021-06-26 10:53:29 +0545

    using useState

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 
 const Login = () => {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [emailIsValid, setEmailIsValid] = useState();
+
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [passwordIsValid, setPasswordIsValid] = useState();
+
+
+    const validateEmailHandler = () => {
+        setEmailIsValid(enteredEmail.includes('@'));
+        // console.log('email must include @')
+    }
+
+    const validatePasswordHandler = () => {
+        setPasswordIsValid(enteredPassword.trim().length > 6);
+        // console.log('password length must be greater than 6');
+    }
+
+
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value)
+    }
+
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    }
+
     return (
         <Card>
-        <form>
+        <form onSubmit={(event)=>submitHandler(event)}>
             <div className={classes.control}>
             <label htmlFor='email'>Email</label>
-            <input type='email' />
+            <input
+                        type='email'
+                        id='email'
+                        value={enteredEmail}
+                        onChange={(event) => emailChangeHandler(event)}
+                        onBlur={()=>validateEmailHandler()}
+                    />
             </div>
 
             <div className={classes.control}>
                 <label htmlFor='password'>Password</label>
-                <input type='password' />
+                    <input
+                        type='password'
+                        id='password'
+                        value={enteredPassword}
+                        onChange={(event) => passwordChangeHandler(event)}
+                        onBlur={()=>validatePasswordHandler()}
+
+
+                    />
 
             </div>
 
