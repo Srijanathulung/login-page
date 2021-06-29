4704a01034862f3eb83c9d7478d3dc0a7d4b3a86
Srijana Thulung <srijana.thulung123@gmail.com>
2021-06-29 11:25:52 +0545

    Added localStorage and useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import './App.css';
 import MainHeader from './components/MainHeader/MainHeader';
 import Login from './components/Login/Login';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './components/Home/Home';
 
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+ 
+
+  useEffect(() => {
+    //getItem will retrieve the data
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedInKey');
+  
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  },[])
 
   const loginHandler = (email, password) => {
+    //1 is represented as true and 0 is false
+    localStorage.setItem('isLoggedInKey', '1');
     setIsLoggedIn(true);
   }
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedInKey');
     setIsLoggedIn(false);
   }
   return (
@@ -20,8 +33,9 @@ function App() {
 
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
-      {!isLoggedIn && <Login onLogin={() => loginHandler()} />}
-      {isLoggedIn && <Home onLogout={logoutHandler}/>}
+        {!isLoggedIn && <Login onLogin={() => loginHandler()} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {/* {isLoggedIn ? <Home onLogout={logoutHandler} /> : <Login onLogin={() => loginHandler()} /> } */}
       </main>
 
     </React.Fragment>
